48e99828ff3a5daf2ce5b68539065e605398c1e4
nmtu91174 <nmtu91174@gmail.com>
2025-12-02 16:49:19 +0700

    header updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,74 @@
 // src/components/Header.jsx
 import React from 'react';
-import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <Navbar bg="white" expand="lg" className="shadow-sm sticky-top">
             <Container>
-                <Navbar.Brand as={Link} to="/" className="fw-bold text-spx fs-3">
-                    CourierXpress <span className="fs-6 text-dark">Logistics</span>
+                <Navbar.Brand as={Link} to="/" className="d-flex flex-column align-items-start">
+                    <span className="fw-bold text-spx fs-3 lh-1">CourierXpress</span>
+                    <span className="fw-bold fs-10 text-dark lh-1">Logistics</span>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto align-items-center">
-                        <Nav.Link as={Link} to="/" className="fw-medium">Trang chủ</Nav.Link>
-                        <Nav.Link as={Link} to="/about" className="fw-medium">Về chúng tôi</Nav.Link>
-                        <Nav.Link as={Link} to="/tracking" className="fw-medium">Tra cứu</Nav.Link>
 
-                        {/* Nút đăng nhập/đăng ký cho Admin/Agent/User */}
+                        <Nav.Link as={Link} to="/" className="fw-bold fs-10 text-dark">Home</Nav.Link>
+
+                        <NavDropdown title="Shipping" id="basic-nav-dropdown" className="fw-bold fs-10 text-dark">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+
+                        <NavDropdown title="Services" id="basic-nav-dropdown" className="fw-bold fs-10 text-dark">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+
+                        <NavDropdown title="Become a Partner" id="basic-nav-dropdown" className="fw-bold fs-10 text-dark">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+
+                        <NavDropdown title="Help Center" id="basic-nav-dropdown" className="fw-bold fs-10 text-dark">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+
                         <Button as={Link} to="/login" variant="outline-danger" className="ms-3 rounded-pill px-4">
-                            Đăng nhập
+                            Login
                         </Button>
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
